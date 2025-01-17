bbf48d420db95f355e47bccb631b38cc1b9d3ba3
Danh1211 <160509707+Danh1211@users.noreply.github.com>
2025-01-17 13:09:06 +0700

    Add files via upload

---
+++ DemoTCPService/ClientApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Sockets;
+
+namespace ClientApp
+{
+    internal class Program
+    {
+        static void ConnectServer(String server, int port)
+        {
+            string message, responseData;
+            int bytes;
+            try
+            {
+                //creat a TcpClient
+                TcpClient client = new TcpClient(server, port);
+                Console.Title = "Client Application";
+                NetworkStream stream = null;
+                while (true)
+                {
+                    Console.WriteLine("Input message <press Enter to exit>:");
+                    message = Console.ReadLine();
+                    if (message == string.Empty)
+                    {
+                        break;
+                    }
+                    // Translate the passed message into ASCII and store it as a byte array
+                    Byte[] data = System.Text.Encoding.ASCII.GetBytes($"{message}");
+                    //get a client stream for reading and writing
+                    stream = client.GetStream();
+                    //send the message to the connected TcpServer
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine("Sent: {0}", message);
+                    // receive the TcpServer response
+                    // use buffer to store the response bytes
+                    data = new byte[256];
+                    // read the first batch of the TcpServer response bytes
+                    bytes = stream.Read(data, 0, data.Length);
+                    responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                    Console.WriteLine("Received: {0}", responseData);
+                }
+                //shutdown and end connection
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+
+            static void Main(string[] args)
+            {
+                string server = "127.0.0.1";
+                int port = 13000;
+                ConnectServer(server, port);
+            }
+        }
+}
---
+++ DemoTCPService/ServerApp/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+namespace ServerApp
+{
+    internal class Program
+    {
+        static void ProcessMessage(object parm)
+        {
+            string data;
+            int count;
+            try
+            {
+                TcpClient client = parm as TcpClient;
+                //buffer for reding data
+                Byte[] bytes = new Byte[256];
+                //get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+                //loop to receive all the data sent by the client
+                while ((count = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    //translate data bytes to a ASCII string
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, count);
+                    Console.WriteLine($"Received: {data} at {DateTime.Now:t}");
+                    //process the data sent by the client
+                    data = $"{data.ToUpper()}";
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                    //send back a response
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine($"Sent: {data}");
+                }
+                //shutdown and end connextion
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0}", ex.Message);
+                Console.WriteLine("Waiting message...");
+            }
+        }
+
+        static void ExecuteServer(string host, int port)
+        {
+            int Count = 0;
+            TcpListener server = null;
+            try
+            {
+                Console.Title = "Server Application";
+                IPAddress localAddr = IPAddress.Parse(host);
+                server = new TcpListener(localAddr, port);
+                //start listening for client requests
+                server.Start();
+                Console.WriteLine(new string('*', 40));
+                Console.WriteLine("Waiting for a connection...");
+                //Enter the listening loop
+                while (true)
+                {
+                    //Perform a blocking call to accept requests
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine($"Number of client connected:{++Count}");
+                    Console.WriteLine(new string('*', 40));
+                    //create a thread to receive and send message
+                    Thread thread = new Thread(new ParameterizedThreadStart(ProcessMessage));
+                    thread.Start(client);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+            finally
+            {
+                server.Stop();
+                Console.WriteLine("Server stopped. Press any key to exit !");
+            }
+            Console.Read();
+        }
+        static void Main(string[] args)
+        {
+            string host = "127.0.0.1";
+            //set the TCPListener on port 13000
+            int port = 13000;
+            ExecuteServer(host, port);
+        }
+    }
+}
